74f0138a2185cb4d46bc2bbf5bf8193b46c500ea
mhaa87 <47102878+mhaa87@users.noreply.github.com>
2019-05-31 15:32:34 +0200

    Add files via upload

--- frontend/app.js
+++ frontend/app.js
@@ -3,69 +3,133 @@ const app = new Vue({
     data: {
         title: "Spillprosjekt",
         uri: "http://localhost:3030",
-        curTab: "gameList", //'gameList', 'myGames', 'theme'
+        curTab: "newVote", //'itemList', 'myitems', 'results', 'newVote'
         user: {
             name: "",
             password: "",
             votes: [],
         },
         loggedIn: false,
-        themeName: "Tema",
-        newThemeName: "",
-        newGame: {name: "", link: "", user: ""},
-        gameList: [
-            {name: "Spill 3", link: "http://www.google.com", user: "Tester", vote: 0},
-            {name: "Spill 2", link: "http://www.google.com", user: "Tester2", vote: 0},
-            {name: "Spill 1", link: "http://www.google.com", user: "Tester", vote: 0},
+        suggestionsCloses: {date: "2019-05-31", time: "16:00"},
+        votingCloses: {date: "2019-05-31", time: "23:00"},
+        newVote: {
+            title: "",
+            items: [],
+            votes: {},
+            suggestionsCloses: {date: "2019-05-31", time: "16:00"},
+            votingCloses: {date: "2019-05-31", time: "23:00"},
+        },
+        newItem: {name: "", link: "", user: ""},
+        currentVote: 0,
+        itemList: [
+            {name: "Forslag 1", link: "http://www.google.com", user: "Tester", vote: 0},
+            {name: "Forslag 2", link: "http://www.google.com", user: "Tester2", vote: 0},
+            {name: "Forslag 3", link: "http://www.google.com", user: "Tester", vote: 0},
         ],
-        gameScores: false,
+        itemScores: false,
     },
 
     computed:{
-        
+        suggestionsTimeLeft: function() {return this.getTimeLeft("Forslag", this.suggestionsCloses)},
+        votingTimeLeft: function() {return this.getTimeLeft("Stemming", this.votingCloses)},
     },
    
     methods: {
         openLink(link){window.location.href = link;},
 
-        async getGames(){
-            this.gameList = (await axios.post(this.uri + "/getGames")).data;
+        getTimeLeft: function(name, closeDate) {
+            var closeTime = this.parseDateString(closeDate);
+            var timeLeft = Math.round((closeTime - Date.now())/(60*1000));
+            var min = timeLeft % 60;
+            var hours = Math.round(timeLeft / 60) % 24;
+            var days = Math.round(timeLeft / (60*24));
+            // console.log("days: " + days + " hours: " + hours + " min: " + min);
+            if(days > 31) return (name + " avsluttes " + date + " kl." + time);
+            if(days > 0) return (name + " avsluttes om " + days + " dager og " + hours + " timer");
+            if(hours > 0) return (name + " avsluttes om " + hours + " timer og " + min + " minutter");
+            if(min > 0) return (name + " avsluttes om " + min + " minutter");
+            return (name + " er avsluttet");
+        },
+
+        parseDateString: function(closeDate){
+            var year = closeDate.date.substring(0, 4);
+            var month = closeDate.date.substring(5, 7);
+            var day = closeDate.date.substring(8, 10);
+            var hours = closeDate.time.substring(0, 2);
+            var minutes = closeDate.time.substring(3, 5);
+            return (new Date(year, (month-1), day, hours, minutes, 0, 0)).getTime();
+        },
+
+        async getVoteData(){
+            let data = (await axios.post(this.uri + "/getVoteData", {"date": this.currentVote})).data;
+            this.suggestionsCloses = data.suggestionsCloses;
+            this.votingCloses = data.votingCloses;
+            this.title = data.title;
+            this.itemList = data.items;
+            this.newVote.title = data.title;
+            this.newVote.suggestionsCloses = data.suggestionsCloses;
+            this.newVote.votingCloses = data.votingCloses;
+
+        },
+
+        async getItems(){
+            this.itemList = (await axios.post(this.uri + "/getItems", {"date": this.currentVote})).data;
             if(this.loggedIn) await this.getPlayerVotes();
         },
-        async addGame(){
-            if(this.newGame.link.length < 5) this.newGame.link = 'https://www.google.com/search?q=' + this.newGame.name;
-            await axios.post(this.uri + "/addGame", this.newGame);
-            this.newGame.name = "";
-            this.newGame.link = "";
-            this.getGames();
+
+        async addItem(){
+            if(this.newItem.link.length < 5) this.newItem.link = 'https://www.google.com/search?q=' + this.newItem.name;
+            await axios.post(this.uri + "/addItem", {'date': this.currentVote, 'item': this.newItem});
+            this.newItem.name = "";
+            this.newItem.link = "";
+            this.getItems();
         },
 
-        async getTheme(){
-            this.themeName = (await axios.post(this.uri + "/getCurrentTheme")).data;
+        async getTitle(){
+            this.title = (await axios.post(this.uri + "/getVoteTitle", {'date': this.currentVote})).data;
         },
 
-        async newTheme(){
-            var res = await axios.post(this.uri + "/newTheme", {'themeName': this.newThemeName});
+        async getVoteDuration(){
+            this.voteDuration = (await axios.post(this.uri + "/getVoteDuration", {'date': this.currentVote})).data;
+        },
+
+        async createNewVote(){
+            var res = await axios.post(this.uri + "/newVote", {
+                'title': this.newVote.title, 
+                'suggestionsCloses': this.newVote.suggestionsCloses,
+                'votingCloses': this.newVote.votingCloses,
+            });
             if(res.status === 200){
-                this.themeName = this.newThemeName;
-                this.newThemeName = "";
-                this.gameScores = false;
-                this.gameList = [];
+                this.title = this.newVote.title;
+                this.suggestionsCloses = this.newVote.suggestionsCloses;
+                this.votingCloses = this.newVote.votingCloses;
+                this.itemScores = false;
+                this.itemList = [];
+                this.currentVote = (await axios.post(this.uri + "/getCurrentVote")).data;
+            }
+        },
+      
+        async updateVote(){
+            var res = await axios.post(this.uri + "/updateVote", {'date': this.currentVote, 'data': this.newVote});
+             if(res.status === 200){
+                this.title = this.newVote.title;
+                this.suggestionsCloses = this.newVote.suggestionsCloses;
+                this.votingCloses = this.newVote.votingCloses;
             }
         },
         
         async getPlayerVotes(){
-            this.setPlayerVotes((await axios.post(this.uri + "/getVotes", {"user": this.user.name})).data);
+            this.setPlayerVotes((await axios.post(this.uri + "/getVotes", {"date": this.currentVote, "user": this.user.name})).data);
         },
 
         async vote(name, i, vote){
             if(!this.loggedIn) return;
-            var res = await axios.post(this.uri + "/vote", {"user": this.user.name, "game": name, "vote": vote});
-            if(res.status===200) this.$set(this.gameList[i], 'vote', vote);
+            var res = await axios.post(this.uri + "/vote", {"date": this.currentVote, "user": this.user.name, "item": name, "vote": vote});
+            if(res.status===200 && res.data.status !== false) this.$set(this.itemList[i], 'vote', vote);
         },
 
         async getScores(){
-            this.gameScores = (await axios.post(this.uri + "/getScores")).data;
+            this.itemScores = (await axios.post(this.uri + "/getScores", {"date": this.currentVote})).data;
         },
 
         async login(){
@@ -74,20 +138,20 @@ const app = new Vue({
             if(res.status === 403) return; //wrong password
             if(res.status !== 200) return;
             this.loggedIn = true;
-            this.newGame.user = this.user.name;
-            this.getGames();
+            this.newItem.user = this.user.name;
+            this.getItems();
         },
 
-        async deleteGame(name){
-            var res = await axios.post(this.uri + "/deleteGame", {"name": name});
-            if(res.status) await this.getGames();
+        async deleteItem(name){
+            var res = await axios.post(this.uri + "/deleteItem", {"date": this.currentVote, "name": name});
+            if(res.status) await this.getItems();
         },
 
         async setTheme(){await axios.post(this.uri + "/setTheme", {"themeName": this.themeName});},
         
         setPlayerVotes(votes){
-            for(var i=0; i<this.gameList.length; i++){
-                this.$set(this.gameList[i], 'vote', votes[this.gameList[i].name] ? votes[this.gameList[i].name] : 0)
+            for(var i=0; i<this.itemList.length; i++){
+                this.$set(this.itemList[i], 'vote', votes[this.itemList[i].name] ? votes[this.itemList[i].name] : 0)
             }
         },
 
@@ -96,18 +160,21 @@ const app = new Vue({
             this.user.name = "";
             this.user.password = "";
             this.user.votes = [];
-            this.newGame.user = "";
-            this.gameList.forEach(game => {game.vote = 0;});
+            this.newitem.user = "";
+            this.itemList.forEach(item => {item.vote = 0;});
+        },
+
+        async mountApp(){
+            this.currentVote = (await axios.post(this.uri + "/getCurrentVote")).data;
+            await this.getVoteData();
+            //For debug
+            this.user = {name: "Tester", password: "password", votes: [],};
+            await this.login();
         },
     },
 
     mounted() {
-        this.getTheme();
-        this.getGames();
-
-        //For debug
-        this.user = {name: "Tester", password: "password", votes: [],};
-        this.login();
+        this.mountApp();
     }
 })
 
